eb24bd3f37ae046e2755617b7132450f5a79a457
Stanistas1 <stachu.skamrot@gmail.com>
2024-10-24 17:39:11 +0200

    Added ZadanieDomowe2.java

--- src/test/java/pl/stanistas/modul_4/LoginWithValidCredentials.java
+++ src/test/java/pl/stanistas/modul_4/LoginWithValidCredentials.java
@@ -1,8 +1,8 @@
 package pl.stanistas.modul_4;
 
-import com.microsoft.playwright.Locator;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 import com.microsoft.playwright.assertions.PlaywrightAssertions;
+import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.Test;
 import pl.stanistas.common.BaseTest;
 
@@ -12,10 +12,22 @@ class LoginWithValidCredentials extends BaseTest {
     void should_login_with_valid_credentials() {
 
         page.navigate("http://localhost:7080/login");
-        page.fill("id=username", "tomsmith");
+      //  page.pause();
+    /*    page.fill("id=username", "tomsmith");
         page.fill("id=password", "SuperSecretPassword!");
         page.click("#login button");
-
         PlaywrightAssertions.assertThat(page.locator("text=You logged into a secure area!")).isVisible();
+        page.click("//i[contains(text(),'Logout')]");
+       PlaywrightAssertions.assertThat(page.locator("text=You logged out of the secure area!")).isVisible();*/
+
+
+        page.getByLabel("Username").click();
+        page.getByLabel("Username").fill("tomsmith");
+        page.getByLabel("Password").click();
+        page.getByLabel("Password").fill("SuperSecretPassword!");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
+        PlaywrightAssertions.assertThat(page.getByText("You logged into a secure area!")).isVisible();
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Logout")).click();
+        PlaywrightAssertions.assertThat(page.getByText("You logged out of the secure area!")).isVisible();
     }
 }
